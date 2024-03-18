6dc1c88f27b893b6461ee32f422f051eb6634430
Horvath Mark <horvath.mark@students.jedlik.eu>
2024-03-18 13:03:49 +0100

    menu editing

--- menu.py
+++ menu.py
@@ -1,10 +1,15 @@
 from os import system
 
 
+def generic_menu(options):
+    for i in range(len(options)):
+        for adat in options:
+            print(f'{i} - {adat}')
+
 def fomenu():
     system('cls')
     print('1 - Játék indítása')
-    print('2 - Inventory megnyitása')
+    print('2 - Inventory megnyitása') 
     print('3 - Pokemonok hozzáadása')
     print('0 - Kilépés')
     answer = int(input('Válasz: '))
@@ -20,3 +25,4 @@ def fomenu():
             input('Betöltés...')
 
 fomenu()
+generic_menu()
