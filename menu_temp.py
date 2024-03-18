dc0b19c9b9e4a4e75f5afc20803e2ddec516373e
csecsoliver <113012199+csecsoliver@users.noreply.github.com>
2024-03-18 13:04:33 +0100

    menu_temp

--- menu_temp.py
+++ menu_temp.py
@@ -15,26 +15,7 @@ def intro():
 def display_menu(stats: Stats, dialog: str, options: list[str], line_length_override: list[int] | None = None):
     print("\x1B[2J")
     print("-" * 61)
-    location = stats.location
-    location_length = len(location)
-    vehicle = stats.current_vehicle
-    if vehicle is not None:
-        line = vehicle.line
-        location = f"{line.name} -> {vehicle.destination_name()}"
-        location_length = len(location)
-        location = f"{line.color}{location}{colorama.Fore.RESET}"
-    print("│ " + f"{location}" + " " * (52 - location_length) + game_time_to_string(stats.elapsed_time) + " │")
-    print(f"│ Pénz: {stats.money: 17} Ft │ Energia: {stats.stamina: 18}% │")
-    print(f"│ Jegyek: {stats.tickets: 15} db │ Bérlet: {stats.remaining_pass_time: 15} perc │")
-    print(f"│ Cél: {(stats.quest if stats.side_quest is None else stats.side_quest).ljust(52)} │ ")
-    print("-" * 61)
-    dialog_lines = dialog.split("\n")
-    if len(dialog_lines) == 1:
-        dialog_lines.append("")
-    for i, line in enumerate(dialog_lines):
-        length = len(line) if line_length_override is None or len(line_length_override) <= i else line_length_override[i]
-        print(f"│ {line}{" " * (57 - length)} │")
-    print("-" * 61)
+    
     for i in range(5):
         if i < len(options):
             print(f"│ {f"{i + 1}. {options[i]}".ljust(57)} │")
