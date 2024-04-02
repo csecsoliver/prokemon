e4ea80ec363a8310686ef9f5da33915225e2cf41
mohaibalint93 <mohai.balint@students.jedlik.eu>
2024-04-02 22:44:36 +0200

    story teljes leírása bossok pokemonjai biomok struktúra

---
+++ story.py
@@ -0,0 +1,38 @@
+'''
+PLSSSSS EGY KIS VISSZAJELZÉS HOGY MEGFELEL E
+
+4 + 1 biom 
+
+Bármelyik biomból bármelyikbe lehet menni kivéve a végsőbe ott csak ha már az összes boss megvan automatikusan átkerül a játékos
+és vége lesz a játéknak ha legyőzi
+
+Minden biomban 3 megállóhely egy shop egy puszta és egy minibossal tallkoás ami annak a biomnak a trainer minibossa a minibossok 
+nyilván egy nagy boss irányítása alatt vannak aki meg akarja szerezni magának az összes pokemont ami nekünk nem jó és ezt kell 
+megakadályozni hogy mindet legyőzzük nekünk van egy segítőnk (akinek még nincs neve) gyakorlatilag narrátor megy velünk
+végig és meséli hogy mi merre hány méter vakon megbízunk benne de a legvégén kiderül ő mozgatja a szálakat és őt is le kell győznünk
+
+Bossok pokemonjai 25-tel több hp-val és a többi statból 5-tel többel kezdenek mint a pokemonok alapverziója 
+(fix ne legyen túl önnyű legyőzni őket)
+
+- Grassy biom (Normal, Grass, Water, Ground, Poison)
+    - boss pokemonjai: 
+        - Bulbasaur, Pidgey, Psyduck
+
+- Cloud biom (Electric, Ice, Flying, Bug)
+    - boss pokemonjai:
+        - Raichu, Scyther, Zubat
+
+- Mountain biom (Rock, Fighting)
+    - boss pokemonjai:
+        - Aerodactyl, Kabuto, Mankey
+
+- Underworld biom (Fire, Psychic, Ghost, Dragon)
+    - boss pokemonjai:
+        - Arcanine, Dratini, Charmander
+
+- Boss biom (itt már bármilyen pokemon lehet és csak a végső harcra lehet átjutni)
+    - boss pokemonjai: 
+        - Dragonite, Charmeleon, Ivysaur
+    - Volt segítő pokemonjai:
+        - Charizard, Mewtwo, Articuno
+'''
